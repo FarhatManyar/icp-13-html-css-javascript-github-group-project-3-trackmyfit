44f5743cd101c74c8907a09d96e62f6543ab5d85
rutujasontakke26 <sontakkerutuja55@gmail.com>
2026-01-19 23:02:59 +0530

    js for todo list added

---
+++ js/workout-to-do-list.js
@@ -0,0 +1,46 @@
+let todoItems = JSON.parse(localStorage.getItem("todos")) || [
+  { text: "Assignments poori karni hai", done: false },
+  { text: "Leg day", done: false },
+  { text: "Sabji laani hai", done: false }
+];
+
+const container = document.getElementById("todo-items-container");
+const input = document.getElementById("todo-input");
+const addBtn = document.getElementById("add-button");
+
+function renderTodos() {
+  container.innerHTML = "";
+
+  todoItems.forEach((todo, index) => {
+    container.innerHTML += `
+      <div class="todo-item ${todo.done ? "completed" : ""}">
+        <span onclick="toggleDone(${index})">${todo.text}</span>
+        <div class="todo-actions">
+          <img src="/images/delete.png" onclick="deleteTodo(${index})">
+        </div>
+      </div>
+    `;
+  });
+
+  localStorage.setItem("todos", JSON.stringify(todoItems));
+}
+
+addBtn.addEventListener("click", () => {
+  if (!input.value.trim()) return;
+
+  todoItems.push({ text: input.value, done: false });
+  input.value = "";
+  renderTodos();
+});
+
+function deleteTodo(index) {
+  todoItems.splice(index, 1);
+  renderTodos();
+}
+
+function toggleDone(index) {
+  todoItems[index].done = !todoItems[index].done;
+  renderTodos();
+}
+
+renderTodos();
